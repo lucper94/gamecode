ee25364154f8c59fac908d2b56e8fbcfb24343a0
lucper94 <jperez@ticonsulting.com.mx>
2017-05-24 23:42:06 -0500

    alto de imagen y recorte de nombre

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 var App = angular.module('App', []);
 
 App.controller('gamesCtrl', function($scope, $http) {
+
+  $scope.getName = function(name){
+    if(name.length > 25) {
+       return name.substr(0, 25)+'...';
+    }else{
+      return name;
+    } 
+    console.log('hii');
+  }
   $http.get('js/games.json')
        .then(function(res){
           $scope.todos = res.data;                
