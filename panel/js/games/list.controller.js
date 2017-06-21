3d801528f48a3d563f250cdf61ac850f4eb81ee3
lucper94 <jperez@ticonsulting.com.mx>
2017-06-20 22:18:25 -0500

    Se agrego spiner a search

--- panel/js/games/list.controller.js
+++ panel/js/games/list.controller.js
@@ -5,13 +5,14 @@
      function listCtrl(listService){
          
          var ctrl = this;
-
+            ctrl.loading=true;
             listService.getGames()
             .then(function(response){
                 console.log(response);
                 ctrl.gamesObj = response.data.app_data;
                 ctrl.limit = 25;
                 ctrl.predicate = '-SP';
+                ctrl.loading=false;
             })
 
        
