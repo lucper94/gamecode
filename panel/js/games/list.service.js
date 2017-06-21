34899b199aa2da5bb8b3f71d65fc8c89d7e77ed4
Wallace <edhernandezw@gmail.com>
2017-06-21 00:30:26 -0500

    changes

--- panel/js/games/list.service.js
+++ panel/js/games/list.service.js
@@ -3,8 +3,10 @@
      'use strict'
 
         function listService($http){
-            this.getGames = function(){
-                var url ="http://159.203.210.238/metalgear/api/webpage/listing/";
+            this.getGames = function(gamesObj){
+                var Platform = "PS3";
+                var Kind = "With_Box";
+                var url ="http://159.203.210.238/metalgear/api/webpage/listing/" + Platform + "/" + Kind;
                 return $http({
                     method: 'GET',
                     url : url,
