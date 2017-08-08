88aeb147ddfe89ddbf63eee28cb2a13fd36e4ffa
Héctor Abraham Ramírez Torres <developer@cieloytierra.mx>
2017-08-08 11:22:42 -0500

    $Cookie on Detail.

--- js/detail/detail.controller.js
+++ js/detail/detail.controller.js
@@ -2,26 +2,55 @@
 
      'use strict'
 
-     function gameDetail(detailService,$stateParams){
-         var ctrl = this;
+     function gameDetail(detailService,$stateParams,$cookies){
+         
+        var ctrl = this;
 
-         ctrl.gameId = $stateParams.id;
-         angular.element(document).ready(function () {
-            ctrl.resize_header();
-            $( ".game-footer" ).addClass( "footer-detail" );
-         });
-         detailService.getDetail(ctrl.gameId)
+        if($cookies.getObject('detailParams') != undefined){
+            
+            //Cuando se elige una plataforma diferente.
+            if($stateParams.id != null){
+                
+                var detailParams = {
+                    gameID:  $stateParams.id
+                }
+                
+                $cookies.putObject('detailParams', detailParams, {path: "/"});
+            }
+            
+
+        }
+        
+        else{
+
+           var detailParams = {
+                gameID:  241,
+                
+            }
+            
+            $cookies.putObject('detailParams', detailParams, {path: "/"});
+
+        }
+
+        ctrl.gameId = $cookies.getObject('detailParams').gameID;
+        
+        detailService.getDetail(ctrl.gameId)
             .then(function(response){
-                console.log(response);
                 ctrl.gameDetail = response.data;
                 ctrl.loading=false;
             })
+
+        angular.element(document).ready(function(){
+            ctrl.resize_header();
+            $( ".game-footer" ).addClass( "footer-detail" );
+        });
+        
         $(window).resize(function(){
             ctrl.resize_header();
         });
+
         ctrl.resize_header = function(){
             var header= $('.game-header').height()+20;
-            console.log(header);
             $('.game-banner').css('margin-top',header);
             $('.game-footter').css('bottom',0);
         }
