b52d9d0fb378ce782e4eedf817c88d56685edd55
lucper94 <jperez@ticonsulting.com.mx>
2017-08-22 12:34:00 -0500

    estilo margin-top

--- js/search/search.list.controller.js
+++ js/search/search.list.controller.js
@@ -11,10 +11,18 @@
                 .then(function(response){
                     console.log(response);
                     ctrl.gamesObj = response.data.app_data;
+                    ctrl.resize_header();
                 });
         };
-
+        ctrl.resize_header = function(){
+            var header= $('.game-header').height()+10;
+            // console.log(header);
+            $('.game-banner').css('margin-top',header);
+            $('.filters').css('margin-top',header);
+            // console.log('entro');
+        }
         ctrl.searchGames($stateParams.searchString);
+        ctrl.resize_header();
      }
 
      angular
